a544671e43d576ba4d88f7be3f58ca20707300f6
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-10-10 15:46:59 -0600

    Change update function to only update selected value.

--- update.js
+++ update.js
@@ -3,18 +3,39 @@ import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-    const data = JSON.parse(event.body);
+    const data = event.body;
+    //Set variables for update request depending on input:
+    const setVariables = (eventData) => {
+        var expression;
+        var body;
+        var array;
+        if (eventData == 1) {
+            body = { ":referencesRequested": true };
+            expression = "SET referencesRequested = :referencesRequested";
+            array = [expression, body];
+        } else if (eventData == 2) {
+            body = { ":hiddenreposRequested": true };
+            expression = "SET hiddenreposRequested = :hiddenreposRequested";
+            array = [expression, body];
+        } else {
+            body = { ":resumeRequested": true };
+            expression = "SET resumeRequested = :resumeRequested";
+            array = [expression, body];
+        }
+        return array;
+    };
+
+    const expression = setVariables(data)[0];
+    const body = setVariables(data)[1];
+
     const params = {
         TableName: process.env.tableName,
         Key: {
             userId: event.requestContext.identity.cognitoIdentityId,
-            permissionId: event.pathParameters.id
+            permissionsId: event.pathParameters.id
         },
-        UpdateExpression: "SET content = :content, attachment = :attachment",
-        ExpressionAttributeValues: {
-            ":attachment": data.attachment || null,
-            ":content": data.content || null
-        },
+        UpdateExpression: expression,
+        ExpressionAttributeValues: body,
         ReturnValues: "ALL_NEW"
     };
 
