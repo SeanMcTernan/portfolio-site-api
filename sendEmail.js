c66b623c4e0b94b2dd921dcf581351122ddd120e
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-12-07 18:18:06 -0700

    Configure backend to use SES

---
+++ sendEmail.js
@@ -0,0 +1,27 @@
+/*/Send Email using SES/*/
+import handler from "./libs/handler-lib";
+import AWS from "aws-sdk";
+
+const SES = new AWS.SES();
+
+export const main = handler(async (event) => {
+    console.log("event", event);
+    const { to, from, subject, text } = JSON.parse(event.body);
+
+    const params = {
+        Destination: {
+            ToAddresses: [to],
+        },
+        Message: {
+            Body: {
+                Text: { Data: text },
+            },
+            Subject: { Data: subject },
+        },
+        Source: from,
+    };
+
+    await SES.sendEmail(params).promise();
+    return params;
+
+});
